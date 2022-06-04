ef51b2c02b19ef075eff4bc4f9b8efe922c27b72
Pierre Mesure <pierre@mesu.re>
2022-06-04 16:24:46 +0200

    Adapt to 0.25

--- lib/decidim/faceless/version.rb
+++ lib/decidim/faceless/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-faceless version.
   module Faceless
-    VERSION = "0.26.0"
-    DECIDIM_VERSION = "~> 0.26"
+    VERSION = "0.25.0"
+    DECIDIM_VERSION = "~> 0.25"
   end
 end
